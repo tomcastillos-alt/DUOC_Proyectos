b579b16cf267fb758b7ad9424fafb53495c35b16
tomcastillos-alt <tom.castillos@duocuc.cl>
2026-04-24 11:01:31 -0400

    arreglado error absurdo en las cadenas

--- Python/Ej Banco Santader.py
+++ Python/Ej Banco Santader.py
@@ -1,27 +1,25 @@
 print("Bienvenido a la interfaz de Cajeros Automáticos de Banco Santander")
 
 while True:
-    rut = input("Ingrese su RUT (sin puntos ni guion, ej: 12345678k): ").lower() # ponemos todo en minúscula para evitar la temida K
+    
+    rut = input("Ingrese su RUT (sin puntos ni guion, ej: 12345678k): ").lower()
+    str(rut)
 
-    # Súper validación de longitud
-    if len(rut) != 9:
-        print("Error: El RUT debe tener exactamente 9 caracteres.")
+    if len(rut) >= 8 and len(rut) <= 9:
+        digitover = rut[-1]
+
+    if digitover.isdigit() or digitover == 'k':
+        print("Rut válido.")
+
+    else: 
+        print("Rut inválido. Intente nuevamente.")
         continue
 
-    # Extraemos el último digito para cachar si es válido
-    verificador = rut[-1]
-    if not (verificador.isdigit() or verificador == 'k'):
-        print("Error: El dígito verificador no es válido (debe ser número o 'k').")
+
+    clave = str(input(f"Por favor, ingresa tu clave. Esta no debe ser mayor a 10 dígitos: "))
+    if len(clave) != 10 :
+         print(f"Clave errónea. Lo digitado tiene {len(clave)} dígito(s). Intente nuevamente.")
         continue
-
-
-    print("RUT validado correctamente.")
-    
-    # Validamos longitud de clave
-    clave = input("Por favor, ingresa tu clave (10 dígitos): ")
-    
-    if len(clave) == 10:
-        print("Clave correcta. Bienvenido al Banco Santander. ¿Qué desea hacer?")
-        break  # chao usuario
     else:
-        print(f"Error: Clave errónea. Ingresaste {len(clave)} dígitos, no 10. Intente nuevamente desde el RUT.") 
+      print("Clave correcta. Bienvenido")
+      break
